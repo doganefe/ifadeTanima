de9060d48aede2cce351b849cdf94b9c1a1aedaa
Dogan <doganefe2@outlook.com>
2020-05-03 00:56:47 +0300

    Add project files.

---
+++ ifadeTespitEtme/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ifadeTespitEtme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdbtnAnger = new System.Windows.Forms.RadioButton();
+            this.rdbtnFear = new System.Windows.Forms.RadioButton();
+            this.rdbtnComtempt = new System.Windows.Forms.RadioButton();
+            this.rdbtnHappy = new System.Windows.Forms.RadioButton();
+            this.rdbtnSad = new System.Windows.Forms.RadioButton();
+            this.rdbtnSurprise = new System.Windows.Forms.RadioButton();
+            this.rdbtnDisgust = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rdbtnAnger
+            // 
+            this.rdbtnAnger.AutoSize = true;
+            this.rdbtnAnger.Location = new System.Drawing.Point(28, 100);
+            this.rdbtnAnger.Name = "rdbtnAnger";
+            this.rdbtnAnger.Size = new System.Drawing.Size(47, 17);
+            this.rdbtnAnger.TabIndex = 0;
+            this.rdbtnAnger.TabStop = true;
+            this.rdbtnAnger.Text = "sinirli";
+            this.rdbtnAnger.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnFear
+            // 
+            this.rdbtnFear.AutoSize = true;
+            this.rdbtnFear.Location = new System.Drawing.Point(28, 123);
+            this.rdbtnFear.Name = "rdbtnFear";
+            this.rdbtnFear.Size = new System.Drawing.Size(65, 17);
+            this.rdbtnFear.TabIndex = 1;
+            this.rdbtnFear.TabStop = true;
+            this.rdbtnFear.Text = "korkmuş";
+            this.rdbtnFear.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnComtempt
+            // 
+            this.rdbtnComtempt.AutoSize = true;
+            this.rdbtnComtempt.Location = new System.Drawing.Point(28, 146);
+            this.rdbtnComtempt.Name = "rdbtnComtempt";
+            this.rdbtnComtempt.Size = new System.Drawing.Size(72, 17);
+            this.rdbtnComtempt.TabIndex = 2;
+            this.rdbtnComtempt.TabStop = true;
+            this.rdbtnComtempt.Text = "aşağılama";
+            this.rdbtnComtempt.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnHappy
+            // 
+            this.rdbtnHappy.AutoSize = true;
+            this.rdbtnHappy.Location = new System.Drawing.Point(28, 170);
+            this.rdbtnHappy.Name = "rdbtnHappy";
+            this.rdbtnHappy.Size = new System.Drawing.Size(50, 17);
+            this.rdbtnHappy.TabIndex = 5;
+            this.rdbtnHappy.TabStop = true;
+            this.rdbtnHappy.Text = "mutlu";
+            this.rdbtnHappy.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnSad
+            // 
+            this.rdbtnSad.AutoSize = true;
+            this.rdbtnSad.Location = new System.Drawing.Point(28, 193);
+            this.rdbtnSad.Name = "rdbtnSad";
+            this.rdbtnSad.Size = new System.Drawing.Size(54, 17);
+            this.rdbtnSad.TabIndex = 4;
+            this.rdbtnSad.TabStop = true;
+            this.rdbtnSad.Text = "üzgün";
+            this.rdbtnSad.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnSurprise
+            // 
+            this.rdbtnSurprise.AutoSize = true;
+            this.rdbtnSurprise.Location = new System.Drawing.Point(28, 216);
+            this.rdbtnSurprise.Name = "rdbtnSurprise";
+            this.rdbtnSurprise.Size = new System.Drawing.Size(55, 17);
+            this.rdbtnSurprise.TabIndex = 3;
+            this.rdbtnSurprise.TabStop = true;
+            this.rdbtnSurprise.Text = "şaşkın";
+            this.rdbtnSurprise.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnDisgust
+            // 
+            this.rdbtnDisgust.AutoSize = true;
+            this.rdbtnDisgust.Location = new System.Drawing.Point(28, 239);
+            this.rdbtnDisgust.Name = "rdbtnDisgust";
+            this.rdbtnDisgust.Size = new System.Drawing.Size(64, 17);
+            this.rdbtnDisgust.TabIndex = 6;
+            this.rdbtnDisgust.TabStop = true;
+            this.rdbtnDisgust.Text = "tiksinmiş";
+            this.rdbtnDisgust.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(184, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 345);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Fotoğraf Yükle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 37);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Çalıştır";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 366);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rdbtnDisgust);
+            this.Controls.Add(this.rdbtnHappy);
+            this.Controls.Add(this.rdbtnSad);
+            this.Controls.Add(this.rdbtnSurprise);
+            this.Controls.Add(this.rdbtnComtempt);
+            this.Controls.Add(this.rdbtnFear);
+            this.Controls.Add(this.rdbtnAnger);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdbtnAnger;
+        private System.Windows.Forms.RadioButton rdbtnFear;
+        private System.Windows.Forms.RadioButton rdbtnComtempt;
+        private System.Windows.Forms.RadioButton rdbtnHappy;
+        private System.Windows.Forms.RadioButton rdbtnSad;
+        private System.Windows.Forms.RadioButton rdbtnSurprise;
+        private System.Windows.Forms.RadioButton rdbtnDisgust;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ifadeTespitEtme/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using DlibDotNet;
+using Newtonsoft.Json;
+using Rectangle = DlibDotNet.Rectangle;
+
+namespace ifadeTespitEtme
+{
+    public partial class Form1 : Form
+    {
+        string imgYolu;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(open.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                imgYolu = open.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var fd = Dlib.GetFrontalFaceDetector())
+            using (var sp = ShapePredictor.Deserialize("shape_predictor_68_face_landmarks.dat"))
+            {
+                // Load image from file
+                var img = Dlib.LoadImage<RgbPixel>(imgYolu);
+
+                // Detect all faces
+                var faces = fd.Operator(img);
+                int yuzAdeti = 0;
+                //Console.WriteLine("yuzler"+ faces.ToString());
+                foreach (var face in faces)
+                {
+                    yuzAdeti++;
+                    // Find the landmark points for this face
+                    var shape = sp.Detect(img, face);
+                    //Console.WriteLine("ilk yuz icin landmarklar"+ shape.ToString());
+                    // Loop through detected landmarks
+                    string path = @"c:\landmarks.txt";
+                    data dataObjem = new data();
+                    dataObjem.Values = new double[136];
+                    dataObjem.Targets = new double[7];
+                    //List<data> _data = new List<data>();
+                    //_data.Add(new data()
+                    //    {
+
+                    //    });
+
+
+                    for (int i = 0; i < shape.Parts; i++)
+                    {
+                        var point = shape.GetPart((uint)i);
+
+                        dataObjem.Values[i] = point.X;
+
+                        var rect = new Rectangle(point);
+                        Dlib.DrawRectangle(img, rect, color: new RgbPixel(255, 0, 0), thickness: 8);
+
+                        // Console.WriteLine(shape.NativePtr.ToString());
+                    }
+
+
+                    for (int i = 0; i < shape.Parts; i++)
+                    {
+                        //console.writeline(shape.tostring());
+                        var point = shape.GetPart((uint)i);
+
+                        dataObjem.Values[i + 68] = point.Y;
+                        //write string to file
+
+                    }
+                    if (rdbtnAnger.Checked)
+                    {
+                        dataObjem.Targets[0] = 1.0;
+                    }
+                    else if (rdbtnFear.Checked)
+                    {
+                        dataObjem.Targets[1] = 1.0;
+                    }
+                    else if (rdbtnComtempt.Checked)
+                    {
+                        dataObjem.Targets[2] = 1.0;
+                    }
+                    else if (rdbtnHappy.Checked)
+                    {
+                        dataObjem.Targets[3] = 1.0;
+                    }
+                    else if (rdbtnSad.Checked)
+                    {
+                        dataObjem.Targets[4] = 1.0;
+                    }
+                    else if (rdbtnSurprise.Checked)
+                    {
+                        dataObjem.Targets[5] = 1.0;
+                    }
+                    else if (rdbtnDisgust.Checked)
+                    {
+                        dataObjem.Targets[6] = 1.0;
+                    }
+                    string json = JsonConvert.SerializeObject(dataObjem, Formatting.Indented);
+
+                    using (StreamWriter w = File.AppendText(path))
+                    {
+                        w.WriteLine(json);
+                        w.WriteLine(',');
+                    }
+
+                    //system.io.file.writealltext(path, json);
+
+                    //using (streamwriter w = file.appendtext(path))
+                    //{
+                    //    w.writeline(json);
+                    //}
+
+                    //Console.Write(shape.Parts);
+                }
+                // Save the result
+                string imagepath = imgYolu.ToString();
+                imagepath = imagepath.Substring(imagepath.LastIndexOf("\\"));
+                imagepath = imagepath.Remove(0, 1);
+                imagepath.TrimEnd('g');
+                imagepath.TrimEnd('e');
+                imagepath.TrimEnd('p');
+                imagepath.TrimEnd('j');
+                imagepath.TrimEnd('.');
+                string newName = imagepath + "Result.jpg";
+
+                Dlib.SaveJpeg(img, newName);
+
+                pictureBox1.Image = new Bitmap(newName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            }
+        }
+    }
+}
+
+   
