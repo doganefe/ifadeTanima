a2d1a9e0cee90373f27d249df302e2a66872f27b
Doğan EFE <doganefe2@outlook.com>
2020-06-17 15:25:16 +0300

    son halimiz

--- ifadeTespitEtme/Form1.cs
+++ ifadeTespitEtme/Form1.cs
@@ -11,11 +11,14 @@ using System.IO;
 using DlibDotNet;
 using Newtonsoft.Json;
 using Rectangle = DlibDotNet.Rectangle;
+using Syncfusion.XlsIO;
 
 namespace ifadeTespitEtme
 {
     public partial class Form1 : Form
     {
+        int inputCount = 1;
+
         string imgYolu;
         public Form1()
         {
@@ -30,7 +33,11 @@ namespace ifadeTespitEtme
                 pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
                 imgYolu = open.FileName;
             }
+
         }
+
+        DataTable dt = new DataTable("Inputs");
+        DataTable dt2 = new DataTable("Outputs");
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -44,107 +51,124 @@ namespace ifadeTespitEtme
                 var faces = fd.Operator(img);
                 int yuzAdeti = 0;
                 //Console.WriteLine("yuzler"+ faces.ToString());
-                foreach (var face in faces)
+                if (faces.Length > 0)
                 {
-                    yuzAdeti++;
-                    // Find the landmark points for this face
-                    var shape = sp.Detect(img, face);
-                    //Console.WriteLine("ilk yuz icin landmarklar"+ shape.ToString());
-                    // Loop through detected landmarks
-                    string path = @"d:\landmarks.txt";
-                    data dataObjem = new data();
-                    dataObjem.Values = new double[136];
-                    dataObjem.Targets = new double[7];
-                    //List<data> _data = new List<data>();
-                    //_data.Add(new data()
-                    //    {
-
-                    //    });
-
-
-                    for (int i = 0; i < shape.Parts; i++)
+                    foreach (var face in faces)
                     {
-                        var point = shape.GetPart((uint)i);
+                        yuzAdeti++;
+                        // Find the landmark points for this face
+                        var shape = sp.Detect(img, face);
+                        //Console.WriteLine("ilk yuz icin landmarklar"+ shape.ToString());
+                        // Loop through detected landmarks
+                        string path = @"d:\landmarks.txt";
+                        data dataObjem = new data();
+                        dataObjem.Values = new double[136];
+                        dataObjem.Targets = new double[7];
 
-                        dataObjem.Values[i] = point.X;
 
-                        var rect = new Rectangle(point);
-                        Dlib.DrawRectangle(img, rect, color: new RgbPixel(255, 0, 0), thickness: 8);
+                        for (int i = 0; i < shape.Parts; i++)
+                        {
+                            var point = shape.GetPart((uint)i);
 
-                        // Console.WriteLine(shape.NativePtr.ToString());
+                            dataObjem.Values[i] = point.X;
+
+                            var rect = new Rectangle(point);
+                            Dlib.DrawRectangle(img, rect, color: new RgbPixel(255, 0, 0), thickness: 8);
+
+                            // Console.WriteLine(shape.NativePtr.ToString());
+                        }
+
+
+                        for (int i = 0; i < shape.Parts; i++)
+                        {
+                            //console.writeline(shape.tostring());
+                            var point = shape.GetPart((uint)i);
+
+                            dataObjem.Values[i + 68] = point.Y;
+                            //write string to file
+
+                        }
+                        if (rdbtnAnger.Checked)
+                        {
+                            dataObjem.Targets[0] = 1.0;
+                        }
+                        else if (rdbtnFear.Checked)
+                        {
+                            dataObjem.Targets[1] = 1.0;
+                        }
+                        else if (rdbtnComtempt.Checked)
+                        {
+                            dataObjem.Targets[2] = 1.0;
+                        }
+                        else if (rdbtnHappy.Checked)
+                        {
+                            dataObjem.Targets[3] = 1.0;
+                        }
+                        else if (rdbtnSad.Checked)
+                        {
+                            dataObjem.Targets[4] = 1.0;
+                        }
+                        else if (rdbtnSurprise.Checked)
+                        {
+                            dataObjem.Targets[5] = 1.0;
+                        }
+                        else if (rdbtnDisgust.Checked)
+                        {
+                            dataObjem.Targets[6] = 1.0;
+                        }
+                        string json = JsonConvert.SerializeObject(dataObjem, Formatting.Indented);
+
+                        using (ExcelEngine excelEngine = new ExcelEngine())
+                        {
+                            IApplication application = excelEngine.Excel;
+                            application.DefaultVersion = ExcelVersion.Excel2013;
+                            IWorkbook workbook = excelEngine.Excel.Workbooks.Open("C:\\Users\\dodom\\OneDrive\\Masaüstü\\testVeriler.xlsx");
+                            IWorksheet InputWorksheet = workbook.Worksheets[0];
+                            IWorksheet OutputWorksheet = workbook.Worksheets[1];
+
+                            //Import the Object Array to Sheet
+                            InputWorksheet.ImportArray(dataObjem.Values, 1, inputCount, true);
+                            OutputWorksheet.ImportArray(dataObjem.Targets, 1, inputCount, true);
+                            inputCount++;
+
+                            //Saving the workbook to disk in XLSX format
+                            workbook.Save();
+                            //Close the instance of IWorkbook
+                            workbook.Close();
+
+                            //Dispose the instance of ExcelEngine
+                            excelEngine.Dispose();
+
+                        }
+                        //workbook.SaveAs("C:\\Users\\dodom\\OneDrive\\Masaüstü\\veriler.xlsx");
+
+                        using (StreamWriter w = File.AppendText(path))
+                        {
+                            w.WriteLine(json);
+                            w.WriteLine(',');
+                        }
+
+                        //system.io.file.writealltext(path, json);
+
+                        //using (streamwriter w = file.appendtext(path))
+                        //{
+                        //    w.writeline(json);
+                        //}
+                        //Console.Write(shape.Parts);
+                        string imagepath = imgYolu.ToString();
+
+                        string newName = imagepath + "Result.jpg";
+
+                        Dlib.SaveJpeg(img, newName);
+
+                        pictureBox1.Image = new Bitmap(newName);
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
                     }
-
-
-                    for (int i = 0; i < shape.Parts; i++)
-                    {
-                        //console.writeline(shape.tostring());
-                        var point = shape.GetPart((uint)i);
-
-                        dataObjem.Values[i + 68] = point.Y;
-                        //write string to file
-
-                    }
-                    if (rdbtnAnger.Checked)
-                    {
-                        dataObjem.Targets[0] = 1.0;
-                    }
-                    else if (rdbtnFear.Checked)
-                    {
-                        dataObjem.Targets[1] = 1.0;
-                    }
-                    else if (rdbtnComtempt.Checked)
-                    {
-                        dataObjem.Targets[2] = 1.0;
-                    }
-                    else if (rdbtnHappy.Checked)
-                    {
-                        dataObjem.Targets[3] = 1.0;
-                    }
-                    else if (rdbtnSad.Checked)
-                    {
-                        dataObjem.Targets[4] = 1.0;
-                    }
-                    else if (rdbtnSurprise.Checked)
-                    {
-                        dataObjem.Targets[5] = 1.0;
-                    }
-                    else if (rdbtnDisgust.Checked)
-                    {
-                        dataObjem.Targets[6] = 1.0;
-                    }
-                    string json = JsonConvert.SerializeObject(dataObjem, Formatting.Indented);
-
-                    using (StreamWriter w = File.AppendText(path))
-                    {
-                        w.WriteLine(json);
-                        w.WriteLine(',');
-                    }
-
-                    //system.io.file.writealltext(path, json);
-
-                    //using (streamwriter w = file.appendtext(path))
-                    //{
-                    //    w.writeline(json);
-                    //}
-
-                    //Console.Write(shape.Parts);
+                } else
+                {
+                    MessageBox.Show("Yüz bulunamadi...");
                 }
                 // Save the result
-                string imagepath = imgYolu.ToString();
-                imagepath = imagepath.Substring(imagepath.LastIndexOf("\\"));
-                imagepath = imagepath.Remove(0, 1);
-                imagepath.TrimEnd('g');
-                imagepath.TrimEnd('e');
-                imagepath.TrimEnd('p');
-                imagepath.TrimEnd('j');
-                imagepath.TrimEnd('.');
-                string newName = imagepath + "Result.jpg";
-
-                Dlib.SaveJpeg(img, newName);
-
-                pictureBox1.Image = new Bitmap(newName);
-                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-
             }
         }
     }
